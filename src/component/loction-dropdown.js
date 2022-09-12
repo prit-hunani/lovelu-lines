fb2338c277e7b031d0989c6bf0951c25ebabb4d3
Prit Gondaliya <prit.github@gmail.com>
2022-09-12 17:48:18 +0530

    location and icon changes commit by prit

--- src/component/loction-dropdown.js
+++ src/component/loction-dropdown.js
@@ -1,32 +1,41 @@
 import React from "react";
-import Modal from 'react-bootstrap/Modal';
+import Modal from "react-bootstrap/Modal";
 import { FaMapMarkerAlt } from "react-icons/fa";
-import sitelogo from '../img/juvlylogo.webp';
-
+import sitelogo from "../img/juvlylogo.webp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 function MyVerticallyCenteredModal(props) {
-    return (
-      <Modal
-        {...props}
-        size="md"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            <div className="text-center">
-                <img src={sitelogo} className="sitlogo" alt="sitelogo"  width="180" />
-            </div>
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h6>Select a location for one of our clinics!</h6>
-          <p>
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header className="close-btn" closeButton></Modal.Header>
+      <Modal.Body>
+        <Modal.Title id="contained-modal-title-vcenter">
+          <div className="text-center">
+            <img
+              src={sitelogo}
+              className="sitlogo"
+              alt="sitelogo"
+              width="180"
+            />
+          </div>
+        </Modal.Title>
+        <h6>Select a location for one of our clinics!</h6>
+        <p>
           <div className="serach-loction">
-            <div className="map_icons"><FaMapMarkerAlt /></div>    
+            <div className="map_icons">
+              <FaMapMarkerAlt />
+            </div>
             <label>
-                <select>
-                <option value="Downtown Columbus, OH">Downtown Columbus, OH</option>
+              <select>
+                <option value="Downtown Columbus, OH">
+                  Downtown Columbus, OH
+                </option>
                 <option value="Polaris">Polaris</option>
                 <option value="Cincinnati">Cincinnati</option>
                 <option value="Cleveland">Cleveland</option>
@@ -36,44 +45,33 @@ function MyVerticallyCenteredModal(props) {
                 <option value="Vadnais Heights">Vadnais Heights</option>
                 <option value="Eau Claire">Eau Claire</option>
                 <option value="Wausau">Wausau</option>
-                </select>
+              </select>
             </label>
+          </div>
+        </p>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+function Loctiondropdown() {
+  const [modalShow, setModalShow] = React.useState(true);
+  return (
+    <>
+      <div className="search-location" onClick={() => setModalShow(true)}>
+        <div className="map_icon">
+          <FaMapMarkerAlt />
+          <p>Downtown Columbus, OH</p>
+          <FontAwesomeIcon icon={faChevronDown} />
         </div>
-          </p>
-        </Modal.Body>
-      </Modal>
-    );
-  }
-  
-  function Loctiondropdown() {
-    const [modalShow, setModalShow] = React.useState(false);
-  
-    return (
-      <>
-        <div className="serach-loction" onClick={() => setModalShow(true)}>
-            <div className="map_icon"><FaMapMarkerAlt /></div>    
-            <label>
-                <select>
-                <option value="Downtown Columbus, OH">Downtown Columbus, OH</option>
-                <option value="Polaris">Polaris</option>
-                <option value="Cincinnati">Cincinnati</option>
-                <option value="Cleveland">Cleveland</option>
-                <option value="New York">New York</option>
-                <option value="Ft Lauderdale">Ft Lauderdale</option>
-                <option value="Chanhassen">Chanhassen</option>
-                <option value="Vadnais Heights">Vadnais Heights</option>
-                <option value="Eau Claire">Eau Claire</option>
-                <option value="Wausau">Wausau</option>
-                </select>
-            </label>
-        </div>
-  
-        <MyVerticallyCenteredModal
-          show={modalShow}
-          onHide={() => setModalShow(false)}
-        />
-      </>
-    );
-  }
-  
-  export default Loctiondropdown;
+      </div>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+}
+
+export default Loctiondropdown;
